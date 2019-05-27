8393271a926dae9292744ac505880a275894ea8d
Safintim <Sarami96>
2019-05-27 17:15:05 +0300

    Settings for heroku

--- jedi/settings.py
+++ jedi/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bh%2#sgdf=!&t06-kg*cl&3#*)@02s#5u)&2rir-no7t&)+4s^'
-
+# SECRET_KEY = 'bh%2#sgdf=!&t06-kg*cl&3#*)@02s#5u)&2rir-no7t&)+4s^'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'bh%2#sgdf=!&t06-kg*cl&3#*)@02s#5u)&2rir-no7t&)+4s^')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != False
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +119,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 AUTH_USER_MODEL = 'jedi_department.Candidate'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
